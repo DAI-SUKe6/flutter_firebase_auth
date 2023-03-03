6e4756b1a4801904c4e65b2d169ed1f179c054e0
จิรภัทร ภูหลักถิน <jirapat.phoolakthin@kkumail.com>
2023-03-03 19:20:47 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,103 @@
+import 'dart:developer';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase_auth/screens/login_screen.dart';
+import 'package:flutter_firebase_auth/services/auth_service.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+AuthService _service = AuthService();
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    User? currentUser = _service.user;
+    String displayEmail = "";
+    if (currentUser != null && currentUser.email != null) {
+      displayEmail = currentUser.email!;
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+                decoration: const BoxDecoration(color: Colors.blue),
+                child: Text("Hello User $displayEmail")),
+            ListTile(
+              title: const Text("Logout"),
+              onTap: () {
+                //call logout()
+                _service.logout();
+                //redirect to login page
+                Navigator.pop(context);
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (context) => LoginScreen()));
+              },
+            )
+          ],
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase_auth/main.dart';
+import 'package:flutter_firebase_auth/screens/create_account_screen.dart';
+
+class LoginScreen extends StatelessWidget {
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login to app"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(100.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(hintText: "Email"),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: const InputDecoration(hintText: "Password"),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+                //call service.login
+                //check result and open new screen
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const MyHomePage(title: "My App")));
+                },
+                child: const Text("Login")),
+            const SizedBox(
+              height: 20,
+            ),
+            GestureDetector(
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => CreateAccountScreen()));
+                },
+                child: const Text("No account? create new >>"))
+          ],
+        ),
+      ),
+    );
+  }
+}
